90e94cc7c9dfca5e01afcea6f7164b486c458b41
Alex Kashie <captainlalas22@gmail.com>
2023-12-14 15:33:58 -0500

    ...

--- Working_with_files.R
+++ Working_with_files.R
@@ -10,3 +10,17 @@ View(Employee_Data)
 ?read.csv
 ?barplot
 ?pie
+
+
+# Binomial distribution
+# We had a questionnaire of 25 questions with one correct answer out of 5 for each question
+# we want to know the prob of having 20 correct answers
+dbinom(20, 25, prob = 0.2) #P(X=20)
+
+# Let's look into normal distribution
+qnorm(p=0.25, mean=75, sd=5, lower.tail = TRUE)
+
+x <- seq(from=55, to=95, by=.25)
+dens <- dnorm(x, mean=75, sd=5)
+plot(x, dens, type="l", main = "X~Nomal with mean=75 and SD=5", xlab = "x", ylab = "Probability density", las=1)
+abline(v=75)
